bbcbbcc0521ec458c08a6f545bc0e2758bd7ee80
dharmesh854 <dharmesh183345@gmail.com>
2025-07-16 03:30:37 -0700

    Update task.py

--- task.py
+++ task.py
@@ -7,7 +7,7 @@ while True:
     print("3. Left-angled Triangle")
     print("Analyze a Range of numbers")
 
-    option = int(input{"Enter your choice(1/2/3/4):"})
+    option = input("Enter your choice(1/2/3/4):")
     if option == "1":
         for i in range(1, 6):
             for j in range(1, i+1):
@@ -17,11 +17,11 @@ while True:
         for a in range(1, 5+1):
             print(" " * (5 - 1), end=" ")
             print("* " * (2 * i -1))
-    elif choice == "3":
+    elif option == "3":
         for i in range(1, 5 + 1):
             print(" " * (5 - 1), end=" ")
             print("* " * i)
-    elif choice == "4":
+    elif option == "4":
         a_range = int(input("Enter your starting range: "))
         b_range = int(input("Enter your ending range: "))
         for i in range(a_range, b_range+1):
@@ -32,7 +32,7 @@ while True:
                 print(f"Number {i} is Even")
         total = (a_range + b_range)/2
         print(int(total))
-    elif choice == "q":
+    elif option == "q":
         print("Program Dismissed!")
         break
     else:
